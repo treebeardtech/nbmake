fe68eeac83522b1048973eb346c799406336a38b
alex-treebeard <alex@treebeard.io>
2020-12-11 10:59:00 +0000

    fix pre-commit

--- setup.py
+++ setup.py
@@ -12,12 +12,14 @@ package_data = \
 
 install_requires = \
 ['PyYAML>=5.3.1,<6.0.0',
- 'jupyter-book[html]>=0.8.3,<0.9.0',
  'nbclient<0.4',
  'nbformat>=5.0.8,<6.0.0',
  'pathlib>=1.0.1,<2.0.0',
  'pydantic>=1.7.2,<2.0.0',
  'pytest>=6.1.2,<7.0.0']
+
+extras_require = \
+{'html': ['jupyter-book>=0.8.3,<0.9.0']}
 
 entry_points = \
 {'pytest11': ['nbmake = nbmake.pytest_plugin']}
@@ -26,7 +28,7 @@ setup_kwargs = {
     'name': 'nbmake',
     'version': '0.0.1',
     'description': 'Pytest plugin for releasing notebooks',
-    'long_description': '# nbmake [Under Construction]\n[![codecov](https://codecov.io/gh/treebeardtech/nbmake/branch/main/graph/badge.svg?token=9GuDM35FuO)](https://codecov.io/gh/treebeardtech/nbmake)\n[![PyPI version](https://badge.fury.io/py/nbmake.svg)](https://badge.fury.io/py/nbmake)\n\nPytest plugin for testing notebooks\n\n## Functionality\n\n1. Runs notebooks individually through [jupyter-book](https://github.com/executablebooks/jupyter-book), allowing the same options such as allowing exceptions on individual cells.\n2. Works everywhere pytest does, allowing testing locally, on pre-commit, and in the cloud\n3. Optionally builds an HTML report of the test run which can be uploaded to hosting providers such as Netlify.\n\nSee [docs](https://treebeardtech.github.io/nbmake) to get started.\n\n### HTML Report Example\n\n![HTML Report](docs/screen.png)\n\n\n\n## Developing\n\n### Install local package\n```\npoetry install\n```\n\n### Activate shell\n```\npoetry shell\n```\n\n### Run static checks\n```\npre-commit run --all-files\npre-commit install\n```\n\n### Run tests\n```\npytest\n```\n\n',
+    'long_description': '# nbmake [Under Construction]\n[![codecov](https://codecov.io/gh/treebeardtech/nbmake/branch/main/graph/badge.svg?token=9GuDM35FuO)](https://codecov.io/gh/treebeardtech/nbmake)\n[![PyPI version](https://badge.fury.io/py/nbmake.svg)](https://badge.fury.io/py/nbmake)\n\nPytest plugin for testing notebooks\n\n## Functionality\n\n1. Runs notebooks individually through [jupyter-book](https://github.com/executablebooks/jupyter-book), allowing the same options such as allowing exceptions on individual cells.\n2. Works everywhere pytest does, allowing testing locally, on pre-commit, and in the cloud\n3. Optionally builds an HTML report of the test run which can be uploaded to hosting providers such as Netlify.\n\nSee [docs](https://treebeardtech.github.io/nbmake) to get started.\n\n### HTML Report Example\n\n![HTML Report](docs/screen.png)\n\n\n\n## Developing\n\n### Install local package\n```\npoetry install -E html\n```\n\n### Activate shell\n```\npoetry shell\n```\n\n### Run static checks\n```\npre-commit run --all-files\npre-commit install\n```\n\n### Run tests\n```\npytest\n```\n\n',
     'author': 'alex-treebeard',
     'author_email': 'alex@treebeard.io',
     'maintainer': None,
@@ -36,6 +38,7 @@ setup_kwargs = {
     'packages': packages,
     'package_data': package_data,
     'install_requires': install_requires,
+    'extras_require': extras_require,
     'entry_points': entry_points,
     'python_requires': '>=3.6.1,<4.0.0',
 }
